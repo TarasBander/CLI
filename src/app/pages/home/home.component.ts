845e27999046e61b4fea19540ce376543a5a3046
rband <rombander@gmail.com>
2019-04-08 22:28:56 +0300

    main components with ngFOr

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,4 +5,14 @@ import { Component} from '@angular/core';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent {
+    pages = [
+        {header: "NEW ARRIVALS", text: "REVIVE YOUR WARDROBE WITH CHIC KNITS", btn: "TAKE A LOOK", classes: "col-lg-6 col-sm-12"},
+        {header: "TUXEDO", text: "REVIVE YOUR WARDROBE WITH CHIC KNITS", btn: "TAKE A LOOK", classes: "col-lg-6 col-sm-12"},
+        {header: "SWEATER", text: "REVIVE YOUR WARDROBE WITH CHIC KNITS", btn: "SHOP NOW", classes: "col-lg-8 col-sm-12"},
+        {header: "TREKKING SHOES", text: "", btn: "", classes: "col-lg-4 col-sm-12"},
+        {header: "CASUAL GLASSES", text: "", btn: "", classes: "col-lg-4 col-sm-12"},
+        {header: "FRESH LOOK T-SHIRT", text: "", btn: "SHOP NOW", classes: "col-lg-4 col-sm-12"},
+        {header: "ELEGANT WATCHES", text: "", btn: "", classes: "col-lg-4 col-sm-12"}
+    ]
+ }
