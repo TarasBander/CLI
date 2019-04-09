633158ea3a48edf7a2035f53008eda6a527e809f
taras.banderovskyi <taras-yurii.banderovskyi@eleks.com>
2019-04-09 08:54:32 +0300

    add http request

--- src/app/pages/arrivals/arrivals.component.ts
+++ src/app/pages/arrivals/arrivals.component.ts
@@ -1,7 +1,17 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
   
 @Component({
     selector: 'arrivals',
-    templateUrl: './arrivals.component.html'
+    templateUrl: './arrivals.component.html',
+    styleUrls: ['./arrivals.component.scss']
 })
-export class ArrivalsComponent { }
+export class ArrivalsComponent { 
+    data: any;
+
+    constructor(private http: HttpClient){}
+
+    ngOnInit(){       
+        this.http.get('https://swapi.co/api/people/').subscribe((data:any) => this.data = data);
+    }
+}
